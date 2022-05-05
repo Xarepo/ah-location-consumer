1b239626d37aea9e9d4587a7241e1eda0a47d1a5
Nicklas Nyström <nicklas.nystrom@xarepo.com>
2022-05-05 10:24:40 +0200

    Cleaning up

--- backend/src/main/java/eu/arrowhead/application/skeleton/consumer/service/LocationService.java
+++ backend/src/main/java/eu/arrowhead/application/skeleton/consumer/service/LocationService.java
@@ -17,70 +17,68 @@ import eu.arrowhead.common.exception.ArrowheadException;
 @Service
 public class LocationService {
 
-    //=================================================================================================
+	//=================================================================================================
 	// members
-	
-    @Autowired
+
+	@Autowired
 	private ArrowheadService arrowheadService;
 
-    //=================================================================================================
+	//=================================================================================================
 	// methods
 
-    public Coordinates fetchLocation() {
+	public Coordinates fetchLocation() {
 
-        final Builder orchestrationFormBuilder = arrowheadService.getOrchestrationFormBuilder();
-    	
-    	final ServiceQueryFormDTO requestedService = new ServiceQueryFormDTO();
-    	requestedService.setServiceDefinitionRequirement("location");
-    	
-    	orchestrationFormBuilder.requestedService(requestedService)
-    							.flag(Flag.MATCHMAKING, false) //When this flag is false or not specified, then the orchestration response cloud contain more proper provider. Otherwise only one will be chosen if there is any proper.
-    							.flag(Flag.OVERRIDE_STORE, false) //When this flag is false or not specified, then a Store Orchestration will be proceeded. Otherwise a Dynamic Orchestration will be proceeded.
-    							.flag(Flag.TRIGGER_INTER_CLOUD, false); //When this flag is false or not specified, then orchestration will not look for providers in the neighbor clouds, when there is no proper provider in the local cloud. Otherwise it will. 
-    	
-    	final OrchestrationFormRequestDTO orchestrationRequest = orchestrationFormBuilder.build();
-    	
-    	OrchestrationResponseDTO response = null;
-    	try {
-    		response = arrowheadService.proceedOrchestration(orchestrationRequest);			
-		} catch (final ArrowheadException ex) {
-			//Handle the unsuccessful request as you wish!
-		}
-    	
-    	//EXAMPLE OF CONSUMING THE SERVICE FROM A CHOSEN PROVIDER
-    	
-    	if (response == null || response.getResponse().isEmpty()) {
-    		//If no proper providers found during the orchestration process, then the response list will be empty. Handle the case as you wish!
-    		System.out.println("Orchestration response is empty");
-    		return null;
-    	}
-    	
-    	final OrchestrationResultDTO result = response.getResponse().get(0); //Simplest way of choosing a provider.
-    	
-    	final HttpMethod httpMethod = HttpMethod.GET;//Http method should be specified in the description of the service.
-    	final String address = result.getProvider().getAddress();
-    	final int port = result.getProvider().getPort();
-    	final String serviceUri = result.getServiceUri();
-    	final String interfaceName = result.getInterfaces().get(0).getInterfaceName(); //Simplest way of choosing an interface.
-    	String token = null;
+		final Builder orchestrationFormBuilder = arrowheadService.getOrchestrationFormBuilder();
 
-    	if (result.getAuthorizationTokens() != null) {
-    		token = result.getAuthorizationTokens().get(interfaceName); //Can be null when the security type of the provider is 'CERTIFICATE' or nothing.
-		}
+		final ServiceQueryFormDTO requestedService = new ServiceQueryFormDTO();
+		requestedService.setServiceDefinitionRequirement("location");
 
-    	final Object payload = null; //Can be null if not specified in the description of the service.
-    	
+		orchestrationFormBuilder.requestedService(requestedService)
+				.flag(Flag.MATCHMAKING, false)
+				.flag(Flag.OVERRIDE_STORE, false)
+				.flag(Flag.TRIGGER_INTER_CLOUD, false);
+
+		final OrchestrationFormRequestDTO orchestrationRequest = orchestrationFormBuilder.build();
+
+		OrchestrationResponseDTO response = null;
 		try {
-			final Map<String, Double> coordinates = arrowheadService.consumeServiceHTTP(Map.class, httpMethod, address, port, serviceUri, interfaceName, token, payload);
-			if (coordinates == null) return null;
+			response = arrowheadService.proceedOrchestration(orchestrationRequest);
+		} catch (final ArrowheadException ex) {
+			ex.printStackTrace();
+		}
+
+		if (response == null || response.getResponse().isEmpty()) {
+			System.out.println("Orchestration response is empty");
+			return null;
+		}
+
+		final OrchestrationResultDTO result = response.getResponse().get(0);
+
+		final HttpMethod httpMethod = HttpMethod.GET;
+		final String address = result.getProvider().getAddress();
+		final int port = result.getProvider().getPort();
+		final String serviceUri = result.getServiceUri();
+		final String interfaceName = result.getInterfaces().get(0).getInterfaceName();
+		String token = null;
+
+		if (result.getAuthorizationTokens() != null) {
+			token = result.getAuthorizationTokens().get(interfaceName);
+		}
+
+		final Object payload = null;
+
+		try {
+			final Map<String, Double> coordinates = arrowheadService.consumeServiceHTTP(Map.class,
+					httpMethod, address, port, serviceUri, interfaceName, token, payload);
+			if (coordinates == null)
+				return null;
 			final double latitude = coordinates.get("latitude");
 			final double longitude = coordinates.get("longitude");
-			System.out.println(coordinates);
 			return new Coordinates(latitude, longitude);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
-    }
-    
+	}
+
 }
